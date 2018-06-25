d48ee75d9b686991890847170e39bbfbb5781579
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2018-06-25 13:23:23 +0100

    Added print function

--- maze_struct.cs
+++ maze_struct.cs
@@ -13,3 +13,12 @@ datatype[] maze;
 string filename;
 
 // Extract maze object from file
+
+
+
+public void PrintMaze(datatype maze_array)
+{
+	if maze_array[0]==1:
+	
+    Console.WriteLine("I'm doing something...");
+}
