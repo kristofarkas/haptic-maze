78e5bc1387ddf152b5b1cd891c7070dd1e1b2db9
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2018-06-25 13:09:28 +0100

    initial structure for maze

---
+++ maze_struct.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using Ultrahaptics;
+
+// Initialise the maze stuff
+
+datatype[] current_position;
+
+datatype[] maze;
+
+string filename;
+
+// Extract maze object from file
