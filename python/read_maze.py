b71d80ad5bf57f52b19386bac49b0bfc0527c11b
Omarj37 <o.jahangir.17@ucl.ac.uk>
2018-06-26 13:19:20 +0100

    added definitions for walls

--- python/read_maze.py
+++ python/read_maze.py
@@ -1,27 +1,31 @@
 maze = []
 with open('str_test.txt') as f:
-	for line in f:
-		line = line.replace("'","")
-		line = line.replace("[", "")
-		line = line.replace("]", "")
-		line = line.replace("\n", "")
-		maze.append(line.split(','))
+    for line in f:
+        line = line.replace("'","")
+        line = line.replace("[", "")
+        line = line.replace("]", "")
+        line = line.replace("\n", "")
+        maze.append(line.split(','))
 
-print(maze)
+#print(maze)
 
 maze_list = []
 
 for i in range(1, len(maze)-1,2):
-	for j in range(1, len(maze[0])-1,2):
+    for j in range(1, len(maze[0])-1,2):
 
-		# Check northern wall 
-		print(0)
-		if maze[i-1][j] ==' O':
-			maze_list.append([i,j,0,1])
-		# Check east wall
-		if maze[i][j+1]==' O':
-			maze_list.append([i,j,1,1])
+        # Check northern wall
+        if maze[i-1][j] ==' O':
+            maze_list.append([i,j,0,1])
+        # Check eastern wall
+        if maze[i][j+1]==' O':
+            maze_list.append([i,j,1,1])
+        #check western wall
+        if maze[i][j-1]==' O':
+            maze_list.append([i,j,3,1])
+        #check southern wall
+        if maze[i+1][j]==' O':
+            maze_list.append([i,j,2,1])
 
-print(maze_list)
-
-
+for i in enumerate(maze_list):
+    print(i)
